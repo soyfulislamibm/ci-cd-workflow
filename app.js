75b6d1c3e93c69406fb3086eedce3795ccd0c10f
Soyful Islam <soyful.islam-cic-uk@ibm.com>
2026-07-31 17:37:36 +0100

    fixed lint error

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  let message = 'Works on my machine.';
+  const message = 'Works on my machine.';
   res.send(message);
 });
 
