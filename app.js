509799cb1014e04ce6fbd96331077dd2a447e1a3
Soyful Islam <soyful.islam-cic-uk@ibm.com>
2026-07-31 17:36:14 +0100

    Lint test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const message = 'Works on my machine.';
+  let message = 'Works on my machine.';
   res.send(message);
 });
 
