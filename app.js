9bfc765c42fad24da3c410bef81b8c76f4775cfe
Soyful Islam <soyful.islam-cic-uk@ibm.com>
2026-07-31 17:33:00 +0100

    ESlint setup

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Works on my machine.');
+  const message = 'Works on my machine.';
+  res.send(message);
 });
 
 module.exports = app;
