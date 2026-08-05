300107b36bf8440ea5df20e912fae493004cd96d
Soyful Islam <soyful.islam-cic-uk@ibm.com>
2026-08-05 15:54:28 +0100

    Updated ssh ec2 and docker

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,15 @@ resource "aws_security_group" "app" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # SSH
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -32,15 +41,18 @@ resource "aws_instance" "app" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.app.id]
+  key_name               = "cicd-dev-key"
 
   user_data = <<-EOF
-    #!/bin/bash
-    dnf update -y
-    dnf install -y docker
+  #!/bin/bash
+  dnf update -y
+  dnf install -y docker
 
-    systemctl enable docker
-    systemctl start docker
-  EOF
+  systemctl enable docker
+  systemctl start docker
+
+  usermod -aG docker ec2-user
+EOF
 
   tags = {
     Name        = "cicd-node-app"
